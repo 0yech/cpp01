a49d3832b1036657d4d8e70e3594d886060bcdfa
nrey <Nelson.rey02@gmail.com>
2025-06-12 21:47:59 +0200

    cpp01 100

--- ex01/Zombie.hpp
+++ ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 20:17:54 by nrey              #+#    #+#             */
-/*   Updated: 2025/05/22 20:50:47 by nrey             ###   ########.fr       */
+/*   Updated: 2025/06/12 21:47:52 by nrey             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ class Zombie
         std::string name;
 
     public:
-        Zombie();   // default constructor
-        ~Zombie();  // default destructor
+        Zombie();
+        ~Zombie();
 
         void setName(std::string name);
-        void announce() const; // const as it doesn't edit any values
+        void announce() const;
 };
 
 Zombie* zombieHorde(int N, std::string name);
